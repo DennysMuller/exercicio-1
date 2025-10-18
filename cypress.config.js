c3bdc75d0ddc0af09d8986762fb9ca9c91856f3a
DennysMuller <dennysmuller@gmail.com>
2025-10-18 10:06:53 -0300

    Ajustado o arquivo para reptir quando falhar

--- cypress.config.js
+++ cypress.config.js
@@ -8,4 +8,12 @@ module.exports = defineConfig({
       require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
+  retries: {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    runMode: 2,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    openMode: 0,
+  }
 });
